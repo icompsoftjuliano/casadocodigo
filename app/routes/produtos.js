dfd228976e757e969d1d2fdc4504f72c2b43c27c
Juliano Ramos <juliano.ramos@icompsoft.com.br>
2018-02-05 16:39:56 -0200

    Verbos http

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,13 +1,32 @@
 module.exports = function (app) {
-    app.get('/produtos', function (request, response) {
+    var listaProdutos = function (request, response) {
 
         var connection = app.infra.connectionFactory();
-        var produtosBanco = new app.infra.ProdutosDAO(connection);
+        var produtosDao = new app.infra.ProdutosDAO(connection);
 
-        produtosBanco.lista(function (err, results) {
+        produtosDao.lista(function (err, results) {
             response.render('produtos/lista', { lista: results });
         });
 
         connection.end();
+    };
+
+    app.get('/produtos', listaProdutos);
+
+    app.get('/produtos/form', function (request, response) {
+
+        response.render('produtos/form');
+    });
+
+    app.post('/produtos', function (request, response) {
+
+        var produto = request.body;
+
+        var connection = app.infra.connectionFactory();
+        var produtosDao = new app.infra.ProdutosDAO(connection);
+        produtosDao.salva(produto, function (err, resultados) {
+
+            response.redirect('/produtos')            
+        });
     });
 } 
